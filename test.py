67d4198f5c0b2cbfba5b73d60c528a43aaaa04c9
KeyListener <872490934@qq.com>
2019-10-15 14:35:48 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,73 +1,61 @@
 import cv2
 import numpy as np
-
-cap=cv2.VideoCapture(0)
+import time
+from time import strftime
+import IPython
 
 def mouseColor(event, x, y, flags, param):
 	if event == cv2.EVENT_LBUTTONDOWN:
 		print(str.upper(out), color[y, x])  #输出图像坐标(x,y)处的HSV的值
 
+def alert(img):
+	print('save image')
+	cv2.imwrite("no_quilt.png",img)
+
+cap = cv2.VideoCapture(0)
+
+x = 664
+y = 292
+h = 200
+w = 400
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+detect_time = 10
+color_len = detect_time * fps
+quilt_color = 100
+colors = [] 
+
+print('Begin detect...')
+
 while True:
-	#从摄像头读取图片
-	sucess,img=cap.read()
+	success,img = cap.read()
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  #变成灰度图
-	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  #变成HSV格式
-	out = 'bgr'
-	color = img[1][700]
-	if color[0] >100 and color[1]>100 and color[2]>100:
-		print('人物在检测区域')
-	else:
-		print('人不在')
-	# out = 'gray'
-	# color = gray
-	# out == 'hsv':
-	# color = hsv
+	# cv2.cvtColor(gray, cv2.COLOR_BGR2HSV)  #变成HSV格式
+	cv2.putText(gray, strftime("%H:%M:%S"), (10,70), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,255,0),2,cv2.LINE_AA)
+	img = cv2.rectangle(gray, (x+w,y+h), (x,y), (255,255,255), 2)
+
 	cv2.namedWindow("Color Picker")
 	cv2.setMouseCallback("Color Picker", mouseColor)
-	#转为灰度图片
-	#显示摄像头，背景是灰度。
 	cv2.imshow("img",gray)
-	#保持画面的持续。
-	k=cv2.waitKey(1)
+
+	color = img[y+2:y+h-2,x+2:x+w-2] # +2,-2 去掉白框,注意shape与img是倒的
+	color_mean = color.mean()
+
+	colors.append(color_mean)
+	if len(colors) == color_len:
+		print('light:',sum(colors)/color_len)
+		if sum(colors)/color_len < quilt_color: 
+			print(strftime("%H:%M:%S"),'No quilt')
+			alert(img)
+			time.sleep(2)
+			break
+		else:
+			colors = []
+			colors.append(color_mean)
+
+	k = cv2.waitKey(1)
 	if k == 27:
 		#通过esc键退出摄像
 		cv2.destroyAllWindows()
 		break
-	elif k==ord("s"):
-		#通过s键保存图片，并退出。
-		cv2.imwrite("image2.jpg",img)
-		cv2.destroyAllWindows()
 
-
-
-# img_BGR = cv2.imread('citrus-fruit-colorful-delicious-2146386.jpg') 
-# print(type(img_BGR)) # numpy.ndarray类型可以用numpy对图像进行各种操作
-# print(img_BGR.dtype) # 图像数据类型，一张图片的像素值范围是[0,255], 因此默认类型是unit8
-# print(img_BGR.shape[0]) # 行
-# print(img_BGR.shape[1]) # 列
-# print(img_BGR.shape[2]) # 通道
-# print(img_BGR.size) # 像素数目
-# print(img_BGR.max()) # 最大像素值
-# print(img_BGR.min()) # 最小像素值
-# print(img_BGR.mean()) #素值平均值
-
-# # 定义鼠标交互函数
-# def mouseColor(event, x, y, flags, param):
-#	 if event == cv2.EVENT_LBUTTONDOWN:
-#		 print(str.upper(out), color[y, x])  #输出图像坐标(x,y)处的HSV的值
-
-# img = cv2.imread('3.PNG')  #读进来是BGR格式
-# # 进行颜色格式的转换
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  #变成灰度图
-# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  #变成HSV格式
-# out = 'bgr'
-# color = img
-# # out = 'gray'
-# # color = gray
-# # out == 'hsv':
-# #	 color = hsv
-# cv2.namedWindow("Color Picker")
-# cv2.setMouseCallback("Color Picker", mouseColor)
-# cv2.imshow("Color Picker", img)
-# if cv2.waitKey(0):
-#	 cv2.destroyAllWindows()
